52d7d05758d4fc988b720a37ef606980af3a8db5
kobra-soft <code.softwaredev@gmail.com>
2024-03-01 13:41:36 +0000

    Commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,15 +2,24 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 import Colors from '@/constants/Colors'
+/* import { MaterialCommunityIcons } from '@expo/vector-icons' */
+/* import { FontAwesome5 } from '@expo/vector-icons' */
+import { Ionicons } from '@expo/vector-icons'
 
 const Layout = () => {
   return (
-    <Tabs screenOptions={{
+    <Tabs 
+    screenOptions={{
       tabBarActiveTintColor: Colors.primary,
+      tabBarLabelStyle: { 
+        fontFamily: 'Cereal-bold',
+      }
       }}>
-        <Tabs.Screen name="index"
+        <Tabs.Screen 
+        name="index"
         options={{
           tabBarLabel: 'Explore',
+          tabBarIcon : ({ color, size }) => <Ionicons name="search" size={size} color={color} />
         }} />
 
     </Tabs>
